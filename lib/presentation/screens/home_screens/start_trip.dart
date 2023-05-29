a67fec2bd05c0bda060a00d18caf91da4074104d
Mervet_Magdy <mervetmagdy28@gmail.com>
2023-05-29 17:32:50 +0300

    make part two of destination trip

--- lib/presentation/screens/home_screens/start_trip.dart
+++ lib/presentation/screens/home_screens/start_trip.dart
@@ -1,205 +1,79 @@
-import 'dart:developer';
-import 'dart:io';
-import 'package:flutter/foundation.dart';
+import 'package:bikesterr/data/models/station_model.dart';
+import 'package:bikesterr/presentation/screens/home_screens/destiation_trip.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_code_scanner/qr_code_scanner.dart';
-
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import  'package:intl/intl.dart';
 import '../../../constants.dart';
 
-class StartTrip extends StatelessWidget {
-  const StartTrip({Key? key}) : super(key: key);
+class StartTrip extends StatefulWidget {
+  const StartTrip({Key? key, required this.stationModel}) : super(key: key);
+  final StationModel stationModel;
 
+  @override
+  State<StartTrip> createState() => _StartTripState();
+}
+
+class _StartTripState extends State<StartTrip> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    addRide();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.orange,
-          title: const Text('Scan QR of Bike')),
-      body:const Center(
-        child:  QRViewExample(),
+      backgroundColor: Colors.orange,
+        title: const Text("Trip Started"),
       ),
-    );
-  }
-}
-
-class QRViewExample extends StatefulWidget {
-  const QRViewExample({Key? key}) : super(key: key);
-
-  @override
-  State<StatefulWidget> createState() => _QRViewExampleState();
-}
-
-class _QRViewExampleState extends State<QRViewExample> {
-  Barcode? result;
-  QRViewController? controller;
-  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
-
-  @override
-  void reassemble() {
-    super.reassemble();
-    if (Platform.isAndroid) {
-      controller!.pauseCamera();
-    }
-    controller!.resumeCamera();
-  }
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
-            flex: 1,
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                          style: const ButtonStyle(
-                            backgroundColor: MaterialStatePropertyAll(Colors.orange)
-                          ),
-                            onPressed: () async {
-                              await controller?.toggleFlash();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getFlashStatus(),
-                              builder: (context, snapshot) {
-                                return Text('Flash: ${snapshot.data}');
-                              },
-                            )),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                            style: const ButtonStyle(
-                                backgroundColor: MaterialStatePropertyAll(Colors.orange)
-                            ),
-                            onPressed: () async {
-                              await controller?.flipCamera();
-                              setState(() {});
-                            },
-                            child: FutureBuilder(
-                              future: controller?.getCameraInfo(),
-                              builder: (context, snapshot) {
-                                if (snapshot.data != null) {
-                                  return Text(
-                                      'Camera facing ${describeEnum(snapshot.data!)}');
-                                } else {
-                                  return const Text('loading');
-                                }
-                              },
-                            )),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                          style: const ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll(Color(0xff616161))
-                          ),
-                          onPressed: () async {
-                            if(controller?.resumeCamera() != null) {
-                              await controller?.pauseCamera();
-                            }else{
-                              await controller?.resumeCamera();
-                            }
-
-                          },
-                          child: const Text('pause',
-                              style: TextStyle(fontSize: 20)),
-                        ),
-                      ),
-                      Container(
-                        margin: const EdgeInsets.all(8),
-                        child: ElevatedButton(
-                          style: const ButtonStyle(
-                              backgroundColor: MaterialStatePropertyAll(Colors.orange)
-                          ),
-                          onPressed: () {
-                            addQrInFireStore();
-                          },
-                          child: const Text('summit',
-                              style: TextStyle(fontSize: 20)),
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+      body: Center(
+        child: Column(
+          children: [
+            const Spacer(flex: 1,),
+            const Image(image:AssetImage('assets/biking.png',),width: 250),
+            const Spacer(flex: 4,),
+            ElevatedButton(
+              style: const ButtonStyle(
+                backgroundColor: MaterialStatePropertyAll(Colors.orange)
               ),
-            ),
-          )
-        ],
+                onPressed: (){
+
+                },
+                child: const Text("     Free Trip    ")),
+            const Spacer(flex: 1,),
+            ElevatedButton(
+                style: const ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.orange)
+                ),
+                onPressed: (){
+                  Get.to(()=>const DestinationTrip());
+                },
+                child: const Text("Direct Station")),
+            const Spacer(flex: 10,),
+
+          ],
+        ),
       ),
     );
   }
-  addQrInFireStore(){
-   qr.add({
-     'qr':result!.code,
-   });
-  }
 
-  Widget _buildQrView(BuildContext context) {
-    // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    var scanArea = (MediaQuery.of(context).size.width < 400 ||
-        MediaQuery.of(context).size.height < 400)
-        ? 150.0
-        : 300.0;
-    // To ensure the Scanner view is properly sizes after rotation
-    // we need to listen for Flutter SizeChanged notification and update controller
-    return QRView(
-      key: qrKey,
-      onQRViewCreated: _onQRViewCreated,
-      overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
-          borderRadius: 10,
-          borderLength: 30,
-          borderWidth: 10,
-          cutOutSize: scanArea),
-      onPermissionSet: (ctrl, p) => _onPermissionSet(context, ctrl, p),
-    );
-  }
-
-  void _onQRViewCreated(QRViewController controller) {
-    setState(() {
-      this.controller = controller;
-    });
-    controller.scannedDataStream.listen((scanData) {
-      setState(() {
-        result = scanData;
-      });
+  addRide(){
+    trip.add({
+      'flag': false,
+      'begin station Name': widget.stationModel.stationName,
+      'begin trip': getCurrentTime(),
+      'end station Name':null.toString(),
+      'end trip':null.toString(),
     });
   }
 
-  void _onPermissionSet(BuildContext context, QRViewController ctrl, bool p) {
-    log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
-    if (!p) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('no Permission')),
-      );
-    }
+  String getCurrentTime(){
+    String datetime = DateTime.now().toString();
+    datetime= DateFormat("MMMM, dd, yyyy").format(DateTime.now());
+    String hourDate = DateFormat("hh:mm:ss a").format(DateTime.now());
+    return '$hourDate  $datetime';
   }
-
-  @override
-  void dispose() {
-    controller?.dispose();
-    super.dispose();
-  }
-
-
-
 }
